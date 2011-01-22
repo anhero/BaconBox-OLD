260b16e7e0d7c1a8307d68342191c74d866291c6
Joe Dupuis <joe.dupuis@dupuisj.com>
2011-01-22 01:32:13 -0500

    Made Sprite inherit from the Renderable class. #Adam Labranche <adam@labranche.co>

--- RedBox/Display/Sprite.cpp
+++ RedBox/Display/Sprite.cpp
@@ -1,1 +1,33 @@
 #include "Sprite.h"
+
+using namespace RedBox;
+
+void Sprite::clean() {
+}
+
+void Sprite::copyFrom(const Sprite& src) {
+    if(this != &src && &src) {
+        clean();
+    } else {
+    }
+}
+
+Sprite::Sprite(): Renderable() {
+}
+
+Sprite::Sprite(const Sprite& src):Renderable(src) {
+    copyFrom(src);
+}
+
+Sprite::~Sprite() {
+    clean();
+}
+
+Sprite& Sprite::operator=(const Sprite& src) {
+    Renderable::operator=(src);
+    copyFrom(src);
+    return *this;
+}
+
+void Sprite::render() {
+}
--- RedBox/Display/Sprite.h
+++ RedBox/Display/Sprite.h
@@ -8,17 +8,47 @@
 #define REDBOX_SPRITE
 
 
-
+#include "Renderable.h"
 
 namespace RedBox{
     /** 
-     * @class Sprite
+     * Represents a sprite.
      * @ingroup Display
-     * 
-     * 
      */
-	class Sprite {
-	
+	class Sprite : public Renderable {
+    private:
+        /**
+         * Resets the sprite. Also frees up all allocated memory.
+         */
+        void clean();
+        /**
+         * Makes the instance a copy of the recieved one.
+         * @param src Sprite to make a copy of.
+         */
+        void copyFrom(const Sprite& src);
+    public:
+        /**
+         * Default constructor.
+         */
+        Sprite();
+        /**
+         * Copy constructor.
+         * @param src Sprite to make a copy of.
+         */
+        Sprite(const Sprite& src);
+        /**
+         * Assignation operator overload.
+         * @param src Sprite to make a copy of.
+         */
+        Sprite& operator=(const Sprite& src);
+        /**
+         * Destructor. Frees up all allocated memory.
+         */
+        virtual ~Sprite();
+        /**
+         * Renders the sprite.
+         */
+        void render();
         void warnVertexBodyOfDeletion();
     };
 }
