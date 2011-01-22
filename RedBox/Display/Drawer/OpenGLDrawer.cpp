bbb3c8d6bbfa8302fd49847840e033a9e163547f
Joe Dupuis <joe.dupuis@dupuisj.com>
2011-01-22 01:31:40 -0500

    Rebuilt the openGLDrawer class with the new parameters. #Pier-Luc Faucher <pl.faucher@gmail.com>

--- RedBox/Display/Drawer/OpenGLDrawer.cpp
+++ RedBox/Display/Drawer/OpenGLDrawer.cpp
@@ -4,25 +4,25 @@
 
 using namespace RedBox;
 
-void OpenGLDrawer::drawShapeWithTextureAndColor(GLfloat * vertexArray, GLfloat * textCoordArray, GLubyte * colorArray, GLuint textureID, int numberOfVertex){	
+void OpenGLDrawer::drawShapeWithTextureAndColor(GLfloat * vertices,GLfloat *  textCoordArray , RenderInfo * renderingInfo, int nbVertices){
 	
 	
-	glColorPointer(4, GL_UNSIGNED_BYTE, 0, colorArray);
+        glColorPointer(4, GL_UNSIGNED_BYTE, 0, renderingInfo->colors);
 	glEnableClientState(GL_COLOR_ARRAY);
-	drawShapeWithTexture(vertexArray, textCoordArray, textureID, numberOfVertex);
+        drawShapeWithTexture(vertices, textCoordArray, renderingInfo->textureID, nbVertices);
 	
 	glDisableClientState(GL_COLOR_ARRAY);
 
 }
 
-void OpenGLDrawer::drawShapeWithTexture(GLfloat * vertexArray, GLfloat * textCoordArray, GLuint textureID, int numberOfVertex){
-	glBindTexture(GL_TEXTURE_2D, textureID);
+void OpenGLDrawer::drawShapeWithTexture(GLfloat * vertices,GLfloat *  textCoordArray , RenderInfo * renderingInfo, int nbVertices){
+	glBindTexture(GL_TEXTURE_2D, renderingInfo->textureID);
 	
 	glEnable(GL_TEXTURE_2D);
 	glEnable(GL_BLEND);
 	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	
-	glVertexPointer(2, GL_FLOAT, 0, vertexArray);
+	glVertexPointer(2, GL_FLOAT, 0, vertices);
 	glEnableClientState(GL_VERTEX_ARRAY);
 	
 	
@@ -30,7 +30,7 @@ void OpenGLDrawer::drawShapeWithTexture(GLfloat * vertexArray, GLfloat * textCoo
 	glTexCoordPointer(2, GL_FLOAT, 0, textCoordArray);
 	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 	
-	glDrawArrays(GL_TRIANGLE_FAN, 0, numberOfVertex);
+	glDrawArrays(GL_TRIANGLE_FAN, 0, nbVertices);
 	
 	
 	glDisableClientState(GL_VERTEX_ARRAY);
--- RedBox/Display/Drawer/OpenGLDrawer.h
+++ RedBox/Display/Drawer/OpenGLDrawer.h
@@ -26,27 +26,30 @@ namespace RedBox{
 	public:
         /**
          * Draw a colored and textured shape 
-         * with the given vertices, texture coordinate, color information and texture ID
+         * with the given vertices, texture coordinate, rendering informations (colors array and textureID) and
+         * number of vertices
          * color information will blend with the textured (and background if alpha is not at max value of 255)
          *
-         * @param vertexArray Array of vertex to draw, must have this form (X1, Y1, X2, Y2, Xn, Yn).
+         * @param vertices Array of vertex to draw, must have this form (X1, Y1, X2, Y2, Xn, Yn).
          * The order must be clock wise
          * @param textCoordArray Array of texture coordinate, every value must be in the range 0 to 1. 
-         * @param colorArray Array of colors. Must have this form (R1, G1, B1, A1, Rn, Gn, Bn, An). Value must be between 0 and 255.
-         * @param textureID OpenGL texture handle.
+         * @param renderingInfo object of type RenderInfo, containing colors array and textureID
+         * @param nbVertices number of vertices to draw
          */
-		static void drawShapeWithTextureAndColor(GLfloat * vertexArray, GLfloat * textCoordArray, GLubyte * colorArray, GLuint textureID, int numberOfVertex);
+                static void drawShapeWithTextureAndColor(GLfloat * vertices, GLFloat * textCoordArray, RenderInfo * renderingInfo, int nbVertices);
         
         /**
          * Draw a textured shape 
-         * with the given vertices, texture coordinate and texture ID
+         * with the given vertices, texture coordinate, rendering informations (textureID) and
+         * the number of vertices
          *
-         * @param vertexArray Array of vertex to draw, must have this form (X1, Y1, X2, Y2, Xn, Yn).
+         * @param vertices Array of vertex to draw, must have this form (X1, Y1, X2, Y2, Xn, Yn).
          * The order must be clock wise
-         * @param textCoordArray Array of texture coordinate, every value must be in the range 0 to 1. 
-         * @param textureID OpenGL texture handle.
+         * @param textCoordArray Array of texture coordinate, every value must be in the range 0 to 1.
+         * @param renderingInfo object of type RenderInfo, containing colors array and textureID
+         * @param nbVertices number of vertices to draw
          */
-		static void drawShapeWithTexture(GLfloat * vertexArray, GLfloat * textCoordArray, GLuint textureID, int numberOfVertex);
+                static void drawShapeWithTexture(GLfloat * vertices,GLFloat * textCoordArray, RenderInfo * renderingInfo, int nbVertices);
 
 	};	
 
