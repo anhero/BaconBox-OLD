ed325262ec3b9ec65debe76a82030ab0dba90b2d
Joe Dupuis <joe.dupuis@dupuisj.com>
2011-01-22 20:27:05 -0500

    creation of the StateDisplay class #Philippe Dubé-Tremblay <philippe@dube-tremblay.com> first version

---
+++ RedBox/other/State.h
@@ -0,0 +1,43 @@
+/**
+ * @file
+ */
+#ifndef __STATE_H
+#define __STATE_H
+
+#include <map>
+#include <list>
+#include <string>
+#include "Renderable.h"
+
+namespace RedBox {
+	/**
+     * A state represent the differents states of THE GAME, 
+	 * it content and manage the renderables objects. Ex: the playState, the MenuState...
+     */
+	class State {
+	public:
+		/**
+         * Adds a renderable object to the multimap to begin rendering and updating it.
+         * @param aRenderable the object to be rendered.
+         */
+		void addRenderable(Renderable* aRenderable);
+		/**
+         * Renders all the objects that need to be rendered.
+         */
+        void render();
+		/**
+         * Deletes, Updates and moves all concerns objects.
+         */
+        void update();
+	private:
+		//Temporarily stores the renderable objects to be deleted.
+        std::list<Renderable*> toDelete;
+		//Temporarily stores the renderable objects that need to change their Z value.
+        std::list<Renderable*> zChange;
+		//Stores all the renderables active renderables objects.
+        std::multimap<int, Renderable*> renderables;
+		//The name of the state.
+        std::string name;
+	}
+}
+#endif
