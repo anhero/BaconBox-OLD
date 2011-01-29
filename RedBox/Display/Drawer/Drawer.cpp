1f420ae43f330c3e69d04f201b0038d8c1bc5b4a
Adam Labranche <adam.labranche@anhero.net>
2011-01-29 10:58:33 -0500

    Added drawer.cpp file : forgot to addremove #Pier-Luc Faucher <pl.faucher@gmail.com>

---
+++ RedBox/Display/Drawer/Drawer.cpp
@@ -0,0 +1,20 @@
+#ifdef RB_OPENGL
+#include "OpenGLDrawer.h"
+#endif
+
+#include "Drawer.h"
+
+
+using namespace RedBox;
+
+void Drawer::drawShapeWithTextureAndColor(float * vertices,float *  textCoordArray , RenderInfo * renderingInfo, int nbVertices){
+#ifdef RB_OPENGL
+    OpenGLDrawer::drawShapeWithTextureAndColor(vertices,textCoordArray,renderingInfo,nbVertices);
+#endif
+}
+
+void Drawer::drawShapeWithTexture(float * vertices,float *  textCoordArray , RenderInfo * renderingInfo, int nbVertices){
+#ifdef RB_OPENGL
+    OpenGLDrawer::drawShapeWithTexture(vertices,textCoordArray,renderingInfo,nbVertices);
+#endif
+}
