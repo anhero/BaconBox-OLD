51368c07cf64ca53f2de07e345a37995b37230ad
Joe Dupuis <joe.dupuis@anhero.net>
2011-01-30 12:06:00 -0500

    Created the AudioEngine abstract class files. #Adam Labranche <adam.labranche@anhero.net>

---
+++ RedBox/Audio/AudioEngine.cpp
@@ -0,0 +1,3 @@
+#include "AudioEngine.h"
+
+using namespace RedBox;
---
+++ RedBox/Audio/AudioEngine.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @ingroup Audio
+ */
+#ifndef RB_AUDIO_ENGINE_H
+#define RB_AUDIO_ENGINE_H
+
+namespace RedBox {
+	class AudioEngine {
+	};
+}
+
+#endif
