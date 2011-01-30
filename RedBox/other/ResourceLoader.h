6b8bac4c901220a1f13307c254a720b0699c54a0
Joe Dupuis <joe.dupuis@anhero.net>
2011-01-30 10:59:39 -0500

    Added the documentation to the ResourceLoader class. #Adam Labranche <adam.labranche@anhero.net>

--- RedBox/other/ResourceLoader.h
+++ RedBox/other/ResourceLoader.h
@@ -13,11 +13,26 @@
 #include "TextureInfo.h"
 
 namespace RedBox {
+	/**
+	 * Class that loads resources like textures and audio.
+	 * @ingroup Display
+	 */
 	class ResourceLoader {
 	public:
+		/**
+		 * Loads a texture from a file and assigns a representative key to it.
+		 * @param filePath Path to the file containing the texture.
+		 * @param key Key used to identify this new texture.
+		 */
 		static void loadTexture(const std::string& filePath, const std::string& key);
+		/**
+		 * Gets the information about the asked texture. Uses the texture's key
+		 * to find it.
+		 * @param key Key used to identify and find the asked texture.
+		 */
 		static TextureInfo getTextures(const std::string& key);
 	private:
+		/// Map associating the textures' keys and their information.
 		static std::map<std::string, TextureInfo> textures;
 	};
 }
