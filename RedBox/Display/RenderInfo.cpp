fbb1e14691536acdc0a1df77dcdcacb58a9b27c6
Joe Dupuis <joe.dupuis@anhero.net>
2011-01-30 14:08:11 -0500

    Moved the function implementations to the new RenderInfo.cpp file. #Adam Labranche <adam.labranche@anhero.net> Renamed the headguards to follow the estabished standards.

--- RedBox/Display/RenderInfo.cpp
+++ RedBox/Display/RenderInfo.cpp
@@ -1,3 +1,20 @@
 #include "RenderInfo.h"
 
 using namespace RedBox;
+
+RenderInfo::RenderInfo(): textureID(0) {
+	color[0] = 0;
+	color[1] = 0;
+	color[2] = 0;
+	color[3] = 0;
+}
+
+RenderInfo::RenderInfo(int newTextureID, int* newColor) {
+	textureID = newTextureID;
+	if(newColor) {
+		color[0] = newColor[0];
+		color[1] = newColor[1];
+		color[2] = newColor[2];
+		color[3] = newColor[3];
+	}
+}
--- RedBox/Display/RenderInfo.h
+++ RedBox/Display/RenderInfo.h
@@ -2,9 +2,8 @@
  * @file
  * @ingroup Display
  */
-
-#ifndef __RENDER_INFO_H
-#define __RENDER_INFO_H
+#ifndef RB_RENDER_INFO_H
+#define RB_RENDER_INFO_H
 
 namespace RedBox {
     /**
@@ -13,35 +12,22 @@ namespace RedBox {
      */
     struct RenderInfo {
         /**
+         * Default constructor.
+         */
+        RenderInfo();
+        /**
+         * Parametrized constructor.
+         * @param newTextureID RenderInfo's texture ID to set.
+         * @param newColor Array to use to set the RenderInfo's color.
+         */
+        RenderInfo(int newTextureID, int* newColor);
+        /**
          * Color, first int is red, second is green, third is blue and last one
          * is alpha.
          */
         int color[4];
         /// Texture ID used for OpenGL
         int textureID;
-        /**
-         * Default constructor.
-         */
-        RenderInfo(): textureID(0) {
-            color[0] = 0;
-            color[1] = 0;
-            color[2] = 0;
-            color[3] = 0;
-        }
-        /**
-         * Parametrized constructor.
-         * @param newTextureID RenderInfo's texture ID to set.
-         * @param newColor Array to use to set the RenderInfo's color.
-         */
-        RenderInfo(int newTextureID, int* newColor) {
-            textureID = newTextureID;
-            if(newColor) {
-                color[0] = newColor[0];
-                color[1] = newColor[1];
-                color[2] = newColor[2];
-                color[3] = newColor[3];
-            }
-        }
     };
 }
 
