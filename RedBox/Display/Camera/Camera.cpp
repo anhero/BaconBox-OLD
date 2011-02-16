4c22ba31a5a276cfca262c618d19767d46da6a1d
Adam Labranche <adam.labranche@anhero.net>
2011-02-16 08:16:02 -0500

    Created the new camera #Joe Dupuis <joe.dupuis@anhero.net>

---
+++ RedBox/Display/Camera/Camera.cpp
@@ -0,0 +1,60 @@
+#include "Camera.h"
+#include "Drawer.h"
+
+using namespace RedBox;
+
+
+Camera::Camera() {
+	x = 0;
+	y = 0;
+	angle = 0;
+	zoomFactor = 1.0f;
+}
+
+void Camera::move(int x, int y) {
+	this->x += x;
+	this->y += y;
+}
+
+void Camera::setPosition(int x, int y) {
+	this->x = x;
+	this->y = y;
+}
+
+int Camera::getX() {
+	return x;
+}
+
+int Camera::getY() {
+	return y;
+}
+
+void Camera::resetAngle() {
+	angle = 0;
+}
+void Camera::setAngle(int angle) {
+	this->angle = angle;
+}
+float Camera::getAngle() {
+	return angle;
+}
+void Camera::rotateLeft(int angle) {
+	this->angle -= angle;
+}
+void Camera::rotateRight(int angle) {
+	this->angle += angle;
+}
+
+void Camera::zoom(float factor) {
+	zoomFactor *= factor;
+}
+void Camera::setZoom(float factor) {
+	zoomFactor = factor;
+}
+void Camera::resetZoom() {
+	zoomFactor = 1.0f;
+}
+
+void Camera::render() {
+	Drawer::prepareScene(-x, -y, angle, zoomFactor);
+}
---
+++ RedBox/Display/Camera/Camera.h
@@ -0,0 +1,90 @@
+/**
+ * @file
+ * @ingroup Display
+ */
+
+#ifndef RB_Camera_H
+#define RB_Camera_H
+
+namespace RedBox{
+	/** 
+	 * Represent the camera. This class prepare the scene to mimic a the effect of a camera. 
+	 * If we "move" this object to the right, every object will shift
+	 * to the left before rendering.
+     * @ingroup Display
+     */
+	class Camera{
+	public:
+		///Default Constructor
+		Camera();
+		
+		/**
+		 * Move the camera
+		 * @param x Number of pixels to add to the current x 
+		 * position of the camera.
+		 * @param y Number of pixels to add to the current y 
+		 * position of the camera.
+		 */
+		void move(int x, int y);
+		
+		/**
+		 * Set the position of the camera.
+		 * @param x Set the x position of the camera.
+		 * @param y Set the y position of the camera.
+		 */
+		void setPosition(int x, int y);
+		
+		///Return the position of the camera on the x axis
+		int getX();
+		
+		///Return the position of the camera on the y axis
+		int getY();
+		
+		///Reset the angle of the camera at the default value: 0
+		void resetAngle();
+		
+		///Set the angle of the camera to the given degree angle.
+		void setAngle(int angle);
+		
+		///Return the angle value of the camera in degree.
+		float getAngle();
+		
+		///Rotate the camera to the left with the given degree value.
+		void rotateLeft(int angle);
+		
+		///Rotate the camera to the right with the given degree value.
+		void rotateRight(int angle);
+		
+		/**
+		 * Multiply the zoom factor by the given value.
+		 * @param factor 1 does nothing, less than 1 zoom out, more than 1 zoom in.
+		 */
+		void zoom(float factor);
+		
+		/**
+		 * Set the zoom factor with the given value.
+		 * @param factor 1 does nothing, less than 1 zoom out, more than 1 zoom in.
+		 */
+		void setZoom(float factor);
+		
+		///Set the zoom factor to the default value: 1.
+		void resetZoom();
+		
+		///Prepare the scene according to the position, angle and zoom factor of the camera.
+		void render();
+	private:
+		///Position on the x axis.
+		int x;
+		
+		///Position on the y axis.
+		int y;
+		
+		///Angle of the camera.
+		int angle;
+		
+		///Zoom factor (1 does nothing, less than 1 zoom out, more than 1 zoom in)
+		float zoomFactor;
+	};
+}
+
+#endif
