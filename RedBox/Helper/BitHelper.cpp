69e3d23a0a23b1677c352bdbbc345b35d24e1a28
Joe Dupuis <joe.dupuis@anhero.net>
2011-03-01 12:58:12 -0500

    Added a few functions in the BitHelper class for endian swapping. #Adam Labranche <adam.labranche@anhero.net>

--- RedBox/Helper/BitHelper.cpp
+++ RedBox/Helper/BitHelper.cpp
@@ -6,3 +6,26 @@ bool BitHelper::isBigEndian() {
 	unsigned char testing[4] = {0x12, 0x34, 0x56, 0x78};
 	return *((uint32_t*)(testing)) == 0x12345678;
 }
+
+void BitHelper::endianSwap(uint16_t& x) {
+    x = (x>>8) | 
+	(x<<8);
+}
+
+void BitHelper::endianSwap(uint32_t& x) {
+    x = (x>>24) | 
+	((x<<8) & 0x00FF0000) |
+	((x>>8) & 0x0000FF00) |
+	(x<<24);
+}
+
+void BitHelper::endianSwap(uint64_t& x) {
+    x = (x>>56) | 
+	((x<<40) & 0x00FF000000000000) |
+	((x<<24) & 0x0000FF0000000000) |
+	((x<<8)  & 0x000000FF00000000) |
+	((x>>8)  & 0x00000000FF000000) |
+	((x>>24) & 0x0000000000FF0000) |
+	((x>>40) & 0x000000000000FF00) |
+	(x<<56);
+}
--- RedBox/Helper/BitHelper.h
+++ RedBox/Helper/BitHelper.h
@@ -4,13 +4,35 @@
  */
 #ifndef RB_BIT_HELPER_H
 #define RB_BIT_HELPER_H
+
+#include <cstdint>
+
 namespace RedBox {
 	/**
 	 * Contains functions related to bits and bytes.
 	 */
 	class BitHelper {
 	public:
+		/**
+		 * Checks if the system is in big endian or not.
+		 * @return True if the system is in big endian, false if not.
+		 */
 		static bool isBigEndian();
+		/**
+		 * Swap endian on a 16 bits variable.
+		 * @param x Variable to have its endian swapped.
+		 */
+		static void endianSwap(uint16_t& x);
+		/**
+		 * Swap endian on a 32 bits variable.
+		 * @param x Variable to have its endian swapped.
+		 */
+		static void endianSwap(uint32_t& x);
+		/**
+		 * Swap endian on a 64 bits variable.
+		 * @param x Variable to have its endian swapped.
+		 */
+		static void endianSwap(uint64_t& x);
 	};
 }
 #endif
