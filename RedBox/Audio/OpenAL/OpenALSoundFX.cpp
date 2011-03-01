5475b5d162de09ba9d4a391d15dec5aa5613216c
Joe Dupuis <joe.dupuis@anhero.net>
2011-03-01 12:55:54 -0500

    Wrote the base for the OpenALSoundFX class. #Adam Labranche <adam.labranche@anhero.net>

--- RedBox/Audio/OpenAL/OpenALSoundFX.cpp
+++ RedBox/Audio/OpenAL/OpenALSoundFX.cpp
@@ -1,3 +1,13 @@
 #include "OpenALSoundFX.h"
 
 using namespace RedBox;
+
+void OpenALSoundFX::play(int nbTimes) {
+}
+
+OpenALSoundFX::OpenALSoundFX(): SoundFX(), state(0), bufferId(0), sourceId(0),
+format(0), freq(0) {
+}
+
+OpenALSoundFX::~OpenALSoundFX() {
+}
--- RedBox/Audio/OpenAL/OpenALSoundFX.h
+++ RedBox/Audio/OpenAL/OpenALSoundFX.h
@@ -5,6 +5,10 @@
 #ifndef RB_OPENAL_SOUNDFX_H
 #define RB_OPENAL_SOUNDFX_H
 
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#include <vector>
+
 #include "SoundFX.h"
 
 namespace RedBox {
@@ -14,9 +18,32 @@ namespace RedBox {
 	 */
 	class OpenALSoundFX: public SoundFX {
 	public:
+		/**
+		 * Plays the sound effect a given number of times.
+		 * @param nbTimes Number of times to play the sound effect. -1 for 
+		 * infinite playing.
+		 */
 		void play(int nbTimes = 1);
 	private:
+		/// State of the sound source.
+		ALint state;
+		/// OpenAL sound buffer ID.
+		ALuint bufferId;
+		/// OpenAL sound source.
+		ALuint sourceId;
+		/// Sound data format.
+		ALenum format;
+		/// Frequency of the sound data.
+		ALsizei freq;
+		/// Sound buffer data.
+		std::vector<char> bufferData;
+		/**
+		 * Default constructor.
+		 */
 		OpenALSoundFX();
+		/**
+		 * Destructor.
+		 */
 		~OpenALSoundFX();
 	};
 }
