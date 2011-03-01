1f8cd96ed2bea9f8acbbe624e5398416afd91223
Joe Dupuis <joe.dupuis@anhero.net>
2011-03-01 12:38:52 -0500

    Writing the base for the OpenALEngine class. #Adam Labranche <adam.labranche@anhero.net>

--- RedBox/Audio/OpenAL/OpenALEngine.cpp
+++ RedBox/Audio/OpenAL/OpenALEngine.cpp
@@ -1,3 +1,74 @@
 #include "OpenALEngine.h"
 
-using namespace std;
+#include "Debug.h"
+
+using namespace RedBox;
+
+ALCchar* OpenALEngine::defaultDevice = NULL;
+
+void OpenALEngine::init() {
+	// We open the device.
+	ALCdevice* device = alcOpenDevice(OpenALEngine::defaultDevice);
+	if(device) {
+		// We create the context.
+		ALCcontext* context = alcCreateContext(device, NULL);
+		if(context) {
+			// We activate the context.
+			if(!alcMakeContextCurrent(context)) {
+				$ECHO("Failed to activate the OpenAL context.");
+				// We close the device here because the destructor does not
+				// close the device if no context was created.
+				alcCloseDevice(device);
+				// We destroy the context here because the destructor does not
+				// destroy the context if the activation failed.
+				alcDestroyContext(context);
+			}
+		} else {
+			$ECHO("Failed to create the OpenAL context.");
+			// We close the device here because the destructor does not
+			// close the device if no context was created.
+			alcCloseDevice(device);
+		}
+	} else {
+		$ECHO("Failed to open the OpenAL audio device.");
+	}
+}
+
+void OpenALEngine::update() {
+}
+
+void OpenALEngine::setDefaultDevice(ALCchar* newDevice) {
+	defaultDevice = newDevice;
+}
+
+OpenALEngine::OpenALEngine(): AudioEngine() {
+}
+
+SoundFX* OpenALEngine::loadSoundFX(const std::string& filePath) {
+	return NULL;
+}
+
+SoundFX* OpenALEngine::loadSoundFX(const SoundInfo& info) {
+	return NULL;
+}
+
+BackgroundMusic* loadBackgroundMusic(const std::string& filePath) {
+	return NULL;
+}
+BackgroundMusic* loadBackgroundMusic(const MusicInfo& info) {
+	return NULL;
+}
+
+OpenALEngine::~OpenALEngine() {
+	// We get the current context.
+	ALCcontext* context = alcGetCurrentContext();
+	// We check if it is valid.
+	if(context) {
+		ALCdevice* device = alcGetContextsDevice(context);
+		alcMakeContextCurrent(NULL);
+		alcDestroyContext(context);
+		if(device) {
+			alcCloseDevice(device);
+		}
+	}
+}
--- RedBox/Audio/OpenAL/OpenALEngine.h
+++ RedBox/Audio/OpenAL/OpenALEngine.h
@@ -5,21 +5,69 @@
 #ifndef RB_OPENAL_ENGINE_H
 #define RB_OPENAL_ENGINE_H
 
+#include <OpenAL/al.h>
+#include <OpenAl/alc.h>
+
 #include <string>
 
 #include "AudioEngine.h"
 
 namespace RedBox {
+	/**
+	 * Audio engine using OpenAL to play the sounds.
+	 * @ingroup Audio
+	 */
 	class OpenALEngine: public AudioEngine {
 	public:
+		/**
+		 * Initializes OpenAL.
+		 */
 		void init();
+		/**
+		 * Updates OpenAL.
+		 */
 		void update();
+		/**
+		 * Sets the default device to use for audio output. Must be called
+		 * before initializing the engine.
+		 */
+		static void setDefaultDevice(ALCchar* newDevice);
 	private:
+		/// Device to load.
+		static ALCchar* defaultDevice;
+		/**
+		 * Default constructor.
+		 */
 		OpenALEngine();
+		/**
+		 * Loads a sound effect from a file. For now, it must be a wav file.
+		 * @param filePath Path to the sound effect's file.
+		 * @return Pointer to the loaded sound effect.
+		 Null if the loading failed.
+		 */
 		SoundFX* loadSoundFX(const std::string& filePath);
+		/**
+		 * Loads a sound effect from information.
+		 * @param info Information about the sound effect to load.
+		 * @return Pointer to the loaded sound effect. Null if the loading
+		 * failed.
+		 */
 		SoundFX* loadSoundFX(const SoundInfo& info);
+		/**
+		 * Loads a background music from a file. For now, it must be a wav file.
+		 * @param filePath Path to the music's file.
+		 * @return Pointer to loaded music. Null if the loading failed.
+		 */
 		BackgroundMusic* loadBackgroundMusic(const std::string& filePath);
+		/**
+		 * Loads a background music from information.
+		 * @param info Information about the music to load.
+		 * @return Pointer to the loaded music. Null if the loading failed.
+		 */
 		BackgroundMusic* loadBackgroundMusic(const MusicInfo& info);
+		/**
+		 * Destructor, closes OpenAL.
+		 */
 		~OpenALEngine();
 	};
 }
