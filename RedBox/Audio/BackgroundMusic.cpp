98cfa47cda9ca3b032364a18ad37e1f1f61dca92
Joe Dupuis <joe.dupuis@anhero.net>
2011-03-01 13:06:18 -0500

    Added some missing method definitions. #Adam Labranche <adam.labranche@anhero.net>

--- RedBox/Audio/BackgroundMusic.cpp
+++ RedBox/Audio/BackgroundMusic.cpp
@@ -4,3 +4,6 @@ using namespace RedBox;
 
 BackgroundMusic::BackgroundMusic() {
 }
+
+BackgroundMusic::~BackgroundMusic() {
+}
--- RedBox/Audio/SoundFX.cpp
+++ RedBox/Audio/SoundFX.cpp
@@ -4,3 +4,6 @@ using namespace RedBox;
 
 SoundFX::SoundFX() {
 }
+
+SoundFX::~SoundFX() {
+}
