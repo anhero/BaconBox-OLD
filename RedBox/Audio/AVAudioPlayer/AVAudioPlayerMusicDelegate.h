34870128535f031c7905f10d9ff8f23d2243177f
Adam Labranche <adam.labranche@anhero.net>
2011-03-17 17:58:43 -0400

    Corrected the objective-C class for iOS's music engine. #Adam Labranche <adam.labranche@anhero.net>

--- RedBox/Audio/AVAudioPlayer/AVAudioPlayerMusicDelegate.h
+++ RedBox/Audio/AVAudioPlayer/AVAudioPlayerMusicDelegate.h
@@ -1,23 +1,40 @@
-#ifndef __IPHONEBACKGROUNDMUSIC_H
-#define __IPHONEBACKGROUNDMUSIC_H
+#ifndef RB_AV_AUDIO_PLAYER_MUSIC_DELEGATE_H
+#define RB_AV_AUDIO_PLAYER_MUSIC_DELEGATE_H
 
 #import <Foundation/Foundation.h>
 #import <AVFoundation/AVFoundation.h>
 #import <AudioToolbox/AudioToolbox.h>
 
-@interface RBIPhoneBackgroundMusic : NSObject <AVAudioPlayerDelegate> {
+typedef enum {
+	NOT_FADING,
+	FADING_IN_PLAY,
+	FADING_IN_RESUME,
+	FADING_OUT_STOP,
+	FADING_OUT_PAUSE
+} FadingState;
+
+@interface AVAudioPlayerMusicDelegate : NSObject <AVAudioPlayerDelegate> {
 	@private
 	AVAudioPlayer* BGMusic;
-	
-	
+	float fadeDelta;
+	float realVolume;
+	FadingState fading;
 }
+@property (readwrite, assign) float fadeDelta;
 - (id)initWithPath:(NSString *)path;
-- (void)play;
-- (void)playInLoop;
-- (void)playInLoopFor:(int)numberOfLoop;
+- (void)stopFading;
+- (void)play:(int)nbLoops;
 - (void)resume;
 - (void)pause;
 - (void)stop;
+- (void)fadeOutStop:(float)time;
+- (void)fadeOutStop;
+- (void)fadeOutPause:(float)time;
+- (void)fadeOutPause;
+- (void)fadeInPlay:(int)nbLoops:(float)time;
+- (void)fadeInPlay;
+- (void)fadeInResume:(float)time;
+- (void)fadeInResume;
 
 @end
 #endif
--- RedBox/Audio/AVAudioPlayer/AVAudioPlayerMusicDelegate.m
+++ RedBox/Audio/AVAudioPlayer/AVAudioPlayerMusicDelegate.m
@@ -3,9 +3,9 @@
 #import "AVAudioPlayerMusicDelegate.h"
 
 
-@implementation RBIPhoneBackgroundMusic
+@implementation AVAudioPlayerMusicDelegate
 
-
+@synthesize fadeDelta;
 
 - (id)initWithPath:(NSString *)path{
 	[super init];
@@ -14,36 +14,130 @@
 			   [[NSURL alloc] initFileURLWithPath:path] error:nil];
 	BGMusic.delegate = self;
 
+	realVolume = BGMusic.volume;
 	
 	return self;
 	}
 
-- (void)play{
-	BGMusic.numberOfLoops = 0;
-	[BGMusic stop];
-	[BGMusic play];
+- (void)stopFading {
+	fading = NOT_FADING;
+	BGMusic.volume = realVolume;
 }
-- (void)playInLoop{
-	BGMusic.numberOfLoops = -1;
-	[BGMusic stop];
-	[BGMusic play];
-}
-- (void)playInLoopFor:(int)numberOfLoop{
-		BGMusic.numberOfLoops = (numberOfLoop > 1? numberOfLoop-1 : 0);
-	[BGMusic stop];
-	[BGMusic play];
+
+- (void)play:(int)nbLoops{
+	if(!BGMusic.playing) {
+		[self stopFading];
+		if (nbLoops < -1) {
+			nbLoops = -1;
+		}
+		BGMusic.numberOfLoops = (nbLoops > -1) ? (nbLoops) : (-1);
+		BGMusic.volume = realVolume;
+		[BGMusic stop];
+		[BGMusic play];
+	}
 }
 - (void)resume{
+	[self stopFading];
 	[BGMusic play];
 }
 - (void)pause{
-[BGMusic pause];
+	[self stopFading];
+	[BGMusic pause];
 }
 - (void)stop{
+	[self stopFading];
 	[BGMusic stop];
 }
 
+- (void)fadeOutStop:(float)time{
+	if(time == 0.0 || BGMusic.volume == 0.0) {
+		[self stop];
+	} else {
+		fading = FADING_OUT_STOP;
+		fadeDelta = realVolume / time;
+		[self fadeOutStop];
+	}
+}
 
+- (void)fadeOutStop{
+	if(fading == FADING_OUT_STOP) {
+		if(BGMusic.volume == 0.0) {
+			[self stop];
+		} else {
+			BGMusic.volume = (BGMusic.volume - fadeDelta < 0.0) ? (0.0) : (BGMusic.volume - fadeDelta);
+			[self performSelector:@selector(fadeOutStop) withObject:nil afterDelay: .1];
+		}
+	}
+}
+
+- (void)fadeOutPause:(float)time{
+	if(time == 0.0 || BGMusic.volume == 0.0) {
+		[self pause];
+	} else {
+		fading = FADING_OUT_PAUSE;
+		fadeDelta = realVolume / time;
+		[self fadeOutPause];
+	}
+}
+
+- (void)fadeOutPause{
+	if(fading == FADING_OUT_PAUSE) {
+		if(BGMusic.volume == 0.0) {
+			[self pause];
+		} else {
+			BGMusic.volume = (BGMusic.volume - fadeDelta < 0.0) ? (0.0) : (BGMusic.volume - fadeDelta);
+			[self performSelector:@selector(fadeOutPause) withObject:nil afterDelay: .1];
+		}
+	}
+}
+
+- (void)fadeInPlay:(int)nbLoops :(float)time {
+	if (BGMusic.volume < realVolume || !BGMusic.playing) {
+		if(time == 0.0) {
+			[self play:nbLoops];
+		} else {
+			fading = FADING_IN_PLAY;
+			if(!BGMusic.playing) {
+				BGMusic.volume = 0.0;
+				BGMusic.numberOfLoops = (nbLoops > -1) ? (nbLoops) : (-1);
+				[BGMusic stop];
+				[BGMusic play];
+			}
+			fadeDelta = realVolume / time;
+			[self fadeInPlay];
+		}
+	}
+}
+
+- (void)fadeInPlay {
+	if(fading == FADING_IN_PLAY && BGMusic.volume < realVolume) {
+		BGMusic.volume = (BGMusic.volume + fadeDelta > realVolume) ? (realVolume) : (BGMusic.volume + fadeDelta);
+		[self performSelector:@selector(fadeInResume) withObject:nil afterDelay: .1];
+	}
+}
+
+- (void)fadeInResume:(float)time{
+	if(BGMusic.volume < realVolume || !BGMusic.playing) {
+		if(time == 0.0) {
+			[self resume];
+		} else {
+			fading = FADING_IN_RESUME;
+			if(!BGMusic.playing) {
+				BGMusic.volume = 0.0;
+				[BGMusic play];
+			}
+			fadeDelta = realVolume / time;
+			[self fadeInResume];
+		}
+	}
+}
+
+- (void)fadeInResume{
+	if(fading == FADING_IN_RESUME && BGMusic.volume < realVolume) {
+		BGMusic.volume = (BGMusic.volume + fadeDelta > realVolume) ? (realVolume) : (BGMusic.volume + fadeDelta);
+		[self performSelector:@selector(fadeInResume) withObject:nil afterDelay: .1];
+	}
+}
 
 #pragma mark BGMusic delegate methode
 
