6943ae6230c09ffc61fecf5561c3f0e56fb2a026
Adam Labranche <adam.labranche@anhero.net>
2011-03-17 17:58:43 -0400

    Created the files for the iPhone's music engine's implementation. #Adam Labranche <adam.labranche@anhero.net>

---
+++ RedBox/Audio/AVAudioPlayer/AVAudioPlayerMusic.h
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @ingroup Audio
+ */
+#ifndef RB_AV_AUDIO_PLAYER_MUSIC_H
+#define RB_AV_AUDIO_PLAYER_MUSIC_H
+
+#include "BackgroundMusic.h"
+
+namespace RedBox {
+	class AVAudioPlayerMusic : public BackgroundMusic {
+	};
+}
+
+#endif
