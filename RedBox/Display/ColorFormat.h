26f2fc4f7efefb4683556ea6d426eee5bf6585c0
Joe Dupuis <joe.dupuis@anhero.net>
2011-03-25 00:59:54 -0400

    Created a ColorFormat enum.

---
+++ RedBox/Display/ColorFormat.h
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * @ingroup Display
+ */
+
+#ifndef RB_COLORFORMAT_H
+#define RB_COLORFORMAT_H
+
+namespace RedBox{
+	/** 
+	 * Enum type representing the color component format.
+	 * Used internally to interpret buffers values.
+     * @ingroup Display
+     */
+	enum ColorFormat{
+		RGBA,
+		Alpha
+	}
+}
+
+#endif
