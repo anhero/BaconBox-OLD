eb33731b311e5a30bfbd54992e0c6779638bbb9b
Adam Labranche <adam@labranche.co>
2011-03-28 07:39:09 -0400

    Fixed if defined() problems.

--- RedBox/Helper/ResourcePathHandler.cpp
+++ RedBox/Helper/ResourcePathHandler.cpp
@@ -4,7 +4,7 @@ using namespace RedBox;
 
 std::string ResourcePathHandler::getResourcePathFor(const std::string& item) {
 	std::string path;
-#if defined(TARGET_IPHONE_SIMULATOR) || defined(TARGET_OS_IPHONE)
+#ifdef RB_IPHONE_PLATFORM
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 	
 	NSString *resourceDirectory = [[NSBundle mainBundle] resourcePath];
@@ -22,14 +22,13 @@ std::string ResourcePathHandler::getDocumentPathFor(const std::string& item) {
 
 std::string ResourcePathHandler::getDocumentPath() {
 	std::string documentPath;
-#if defined(TARGET_IPHONE_SIMULATOR) || defined(TARGET_OS_IPHONE)
+#ifdef RB_IPHONE_PLATFORM
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 	
 	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
 	NSString *documentsDirectory = [paths objectAtIndex:0];
 	documentPath = [documentsDirectory cStringUsingEncoding:NSASCIIStringEncoding];
 	[pool release];
-	
 #endif
 	return documentPath;		
 }
