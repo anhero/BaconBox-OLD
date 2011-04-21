10dca21f52c99911e32b5f7440ca9fa8f74644b0
Adam Labranche <adam.labranche@anhero.net>
2011-04-21 15:45:47 -0400

    Corrected some errors in a sources.cmake.

--- RedBox/Redbox/Helper/libc/Sources.cmake
+++ RedBox/Redbox/Helper/libc/Sources.cmake
@@ -1,11 +1,11 @@
-get_filename_component(OSX_Helper_dir ${CMAKE_CURRENT_LIST_FILE} PATH)
-file(GLOB RedBox_RedBox_Helper_OSX_Headers ${OSX_Helper_dir}/*.h)
-file(GLOB RedBox_RedBox_Helper_OSX_Sources ${OSX_Helper_dir}/*.cpp)
+get_filename_component(libc_Helper_dir ${CMAKE_CURRENT_LIST_FILE} PATH)
+file(GLOB RedBox_RedBox_Helper_libc_Headers ${libc_Helper_dir}/*.h)
+file(GLOB RedBox_RedBox_Helper_libc_Sources ${libc_Helper_dir}/*.cpp)
 
 list(APPEND Private_RedBox_Headers ${RedBox_RedBox_Helper_OSX_Headers})
 
-list(APPEND RedBoxHeaders ${RedBox_RedBox_Helper_OSX_Headers})
-list(APPEND RedBoxSources ${RedBox_RedBox_Helper_OSX_Sources})
+list(APPEND RedBoxHeaders ${RedBox_RedBox_Helper_libc_Headers})
+list(APPEND RedBoxSources ${RedBox_RedBox_Helper_libc_Sources})
 
-source_group(RedBox\\Helper\\OSX  FILES ${RedBox_RedBox_Helper_OSX_Sources} ${RedBox_RedBox_Helper_OSX_Headers})
+source_group(RedBox\\Helper\\libc  FILES ${RedBox_RedBox_Helper_libc_Sources} ${RedBox_RedBox_Helper_libc_Headers})
 
