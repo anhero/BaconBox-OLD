370729ae58f7ed34f51f06ddd62a1a46f0f9e15d
Adam Labranche <adam.labranche@anhero.net>
2011-05-04 15:08:19 -0400

    Added an include file for including OpenGL on different platforms.

--- RedBox/Redbox/Display/Driver/OpenGLDriver.h
+++ RedBox/Redbox/Display/Driver/OpenGLDriver.h
@@ -7,22 +7,9 @@
 
 #include "PlatformFlagger.h"
 
-#if defined(RB_MAC_PLATFORM) && defined(RB_SDL)
-#include <OpenGl/OpenGL.h>
-#endif
-
-#if defined(RB_IPHONE_PLATFORM)
-#import <OpenGLES/ES1/gl.h>
-#import <OpenGLES/ES1/glext.h>
-#elif defined(RB_QT)
-#include <QtOpenGL>
-#elif defined(RB_MAC_PLATFORM)
-#include <OpenGL/gl.h>
-#else
-#include <GL/gl.h>
-#endif
-
 #include "GraphicDriver.h"
+
+#include "RBOpenGL.h"
 
 #include "Color.h"
 
--- RedBox/Redbox/Display/TextureInfo.h
+++ RedBox/Redbox/Display/TextureInfo.h
@@ -9,14 +9,7 @@
 
 #include "PlatformFlagger.h"
 
-#ifdef RB_OPENGL
-#if defined(RB_IPHONE_PLATFORM)
-#import <OpenGLES/ES1/gl.h>
-#import <OpenGLES/ES1/glext.h>
-#elif defined(RB_QT)
-#include <QtOpenGL>
-#endif
-#endif
+#include "RBOpenGL.h"
 
 namespace RedBox {
 	/**
