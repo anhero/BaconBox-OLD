249ceffc3d8fefd4f92335ee6cffd44cf8a1d62d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2011-06-10 10:38:40 -0400

    Documented the SDLMainWindow implementation and MainWindow.

--- RedBox/Redbox/Display/Window/MainWindow.h
+++ RedBox/Redbox/Display/Window/MainWindow.h
@@ -1,3 +1,7 @@
+/**
+ * @file
+ * @ingroup WindowDisplay
+ */
 #ifndef RB_MAIN_WINDOW_H
 #define RB_MAIN_WINDOW_H
 
@@ -6,13 +10,42 @@
 #include <string>
 
 namespace RedBox {
+	/**
+	 * Represents the main window of the application. This
+	 * class is used to control the basic properties of the
+	 * window and control it.
+	 */
 	class MainWindow : public sigly::HasSlots<> {
 	public:
+		/**
+		 * Gets the singleton instance of the currently implemented
+		 * MainWindow.
+		 */
 		static MainWindow& getInstance();
+		/**
+		 * Method called when the engine is initialized.
+		 * It is used to setup the geometry of the window.
+		 *
+		 * @param width The width of the window.
+		 * @param height The height of the window
+		 */
 		virtual void onRedBoxInit(int width, int height)=0;
+		/**
+		 * Activates and opens the window.
+		 */
 		virtual void show()=0;
+		/**
+		 * Changes the caption of the window. This is usually
+		 * the title in the titlebar.
+		 *
+		 * @param caption The text used to replace the title.
+		 */
 		virtual void setCaption(const std::string& caption)=0;
 	protected:
+		/**
+		 * The constructor. When implementing a MainWindow, do any
+		 * initialization in here.
+		 */
 		MainWindow();
 	};
 }
--- RedBox/Redbox/Display/Window/SDL/SDLMainWindow.h
+++ RedBox/Redbox/Display/Window/SDL/SDLMainWindow.h
@@ -1,3 +1,7 @@
+/**
+ * @file
+ * @ingroup WindowDisplay
+ */
 #ifndef RB_SDL_MAIN_WINDOW_H
 #define RB_SDL_MAIN_WINDOW_H
 
@@ -5,14 +9,24 @@
 #include "MainWindow.h"
 
 namespace RedBox {
+	/**
+	 * This is an implementation of MainWindow using SDL.
+	 */
 	class SDLMainWindow : public MainWindow {
 	public:
+		/// Gets the singleton instance of SDLMainWindow.
 		static SDLMainWindow& getInstance();
 		void onRedBoxInit(int width, int height);
 		void show();
 		void setCaption(const std::string& caption);
 	private:
+		/**
+		 * Initializes SDL.
+		 */
 		SDLMainWindow();
+		/**
+		 * Properly quits calling SDL_Quit.
+		 */
 		~SDLMainWindow();
 	};
 }
