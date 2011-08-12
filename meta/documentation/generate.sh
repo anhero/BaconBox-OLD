47253ffcf3bd1de33a3068bb0d770728a65a7dee
Samuel Dionne-Riel <samuel@dionne-riel.com>
2011-08-12 16:06:55 -0400

    Added options to the documentation generation script.

--- meta/documentation/generate.sh
+++ meta/documentation/generate.sh
@@ -13,7 +13,17 @@ function print_help() {
 	local this="${0}"
 	cat <<END_OF_HELP
 Usage:
-	$this [version]
+	$this [options] [version]
+
+The version can be any string, but if it is not given
+or "stable", the version will be calculated using mercurial.
+
+OPTIONS
+	--clean             Will remove generated files beforehand
+	--generate-qhp      Will generate the QHP files for Qt Creator
+	--generate-latex    Will generate the latex files
+	--generate-chm      Will generate the compiled HTML Help file
+	--without-html      Will not generate HTML files.
 
 This script is used to generate the documentation.
 This is an helper to allow parameters for other 
@@ -22,20 +32,53 @@ documentation generation (LaTeX, CHM, QHP).
 END_OF_HELP
 }
 
-#TODO: Better parameters parsing...
-if [[ $# -gt 0 ]]; then
-	if [ $# -gt 1 -o "$1" == "--help" -o "$1" == "-h" ]; then
-		print_help
-		exit 0
-	fi
-	version="$1"
-	print_version="$1"
-else
-	version="stable"
-fi
+###########################################################
+# {{{ Parameters parsing
 
-##############################################
-# Version finding...
+# Default values
+
+OPTIONS=""
+version="stable"
+print_version="$version"
+opt_clean="false"
+
+#Keep a save of the parameters
+FULL_PARAMETERS="$@"
+
+while [ $# -gt 0 ];do
+	case "$1" in
+		--help|-h|'-?'|'/?')
+			print_help
+			exit 0
+			;;
+		--generate-qhp)
+			OPTIONS=( ${OPTIONS[@]} 'GENERATE_QHP="YES"' )
+			;;
+		--generate-latex)
+			OPTIONS=( ${OPTIONS[@]} 'GENERATE_LATEX="YES"' )
+			;;
+		--generate-chm)
+			OPTIONS=( ${OPTIONS[@]} 'GENERATE_HTMLHELP="YES"' )
+			;;
+		--without-html)
+			OPTIONS=( ${OPTIONS[@]} 'GENERATE_HTML="NO"' )
+			;;
+		--clean)
+			opt_clean="true"
+			;;
+		# If all else fails.
+		*)
+			echo "Invalid argument: "$1
+			exit 1
+			;;
+	esac
+	shift;
+done
+
+# }}}
+
+###########################################################
+# {{{ Version finding...
 
 HG=${HG-hg}
 declare -r HG
@@ -57,14 +100,31 @@ if [[ $print_version =~ v[[:digit:]].* ]]; then
 	print_version=${print_version#v}
 fi
 
-echo Generating doc for \"$print_version\";
+# }}}
+
+###########################################################
+# Actual work
+
+# Add the project version to the options
+OPTIONS=( ${OPTIONS[@]} 'PROJECT_NUMBER="'${print_version}'"')
 
 cd $script_dir;
 
-OPTIONS=""
-OPTIONS=`echo "PROJECT_NUMBER=\"${print_version}\""`
+echo "Cleaning previously generated files."
+if [[ $opt_clean == "true" ]] ; then
+	rm -rf generated
+fi
 
-( cat Doxyfile ; echo "$OPTIONS" ) | doxygen -
+echo "OPTIONS:"
+for o in "${OPTIONS[@]}"; do
+	echo "--> " $o
+done
+echo "..."
+
+echo Generating doc for \"$print_version\";
+
+
+( cat Doxyfile ; for o in "${OPTIONS[@]}"; do echo "$o"; done ) | doxygen -
 
 echo -e '\n\n\n'
 echo "...Done!"
