3ac1cfaa10aa978758a3b864866890b6a671c30c
Adam Labranche <adam.labranche@anhero.net>
2011-08-17 11:51:49 -0400

    Made the MathHelper class more generic and added a StaticAssert template and a IsNumber helper template.

--- RedBox/Redbox/Helper/MathHelper.cpp
+++ RedBox/Redbox/Helper/MathHelper.cpp
@@ -1,39 +1,15 @@
 #include "MathHelper.h"
 
-#include <cmath>
-#include <climits>
+namespace RedBox {
+	template <> MathHelper::PI<float>::operator float() { return 3.14159265f; }
+	template <> MathHelper::PI<double>::operator double() { return 3.14159265f; }
+	template <> MathHelper::PI<long double>::operator long double() { return 3.14159265l; }
 
-using namespace RedBox;
+	template <> const float MathHelper::RadiansToDegrees<float>::RADIAN_TO_DEGREE = 57.2957795785523f;
+	template <> const double MathHelper::RadiansToDegrees<double>::RADIAN_TO_DEGREE = 57.2957795785523;
+	template <> const long double MathHelper::RadiansToDegrees<long double>::RADIAN_TO_DEGREE = 57.2957795785523l;
 
-const float MathHelper::PI = 3.141592f;
-const float MathHelper::DEGREE_TO_RADIAN = 0.0174532925f;
-const float MathHelper::RADIAN_TO_DEGREE = 57.2957795785523f;
-
-int MathHelper::nextPowerOf2(int number){
-	if(number == 0) {
-		return 1;
-	}
-
-	--number;
-	for (unsigned int i = 1; i < sizeof(int) * CHAR_BIT; i <<= 1) {
-		number = number | number >> i;
-	}
-	return number + 1;
-}
-
-float MathHelper::modFloat(float dividend, float divisor) {
-	return dividend - (floor(dividend / divisor) * divisor);
-}
-
-float MathHelper::degreesToRadians(float degrees) {
-	return degrees * DEGREE_TO_RADIAN;
-}
-
-float MathHelper::radiansToDegrees(float radians) {
-	return radians * RADIAN_TO_DEGREE;
-}
-
-unsigned int MathHelper::roundUpDivision(unsigned int dividend,
-										 unsigned int divisor) {
-	return ((dividend % divisor) ? (dividend / divisor + 1) : (dividend / divisor));
+	template <> const float MathHelper::DegreesToRadians<float>::DEGREE_TO_RADIAN = 0.0174532925f;
+	template <> const double MathHelper::DegreesToRadians<double>::DEGREE_TO_RADIAN = 0.0174532925;
+	template <> const long double MathHelper::DegreesToRadians<long double>::DEGREE_TO_RADIAN = 0.0174532925l;
 }
--- RedBox/Redbox/Helper/SpriteFactory.cpp
+++ RedBox/Redbox/Helper/SpriteFactory.cpp
@@ -9,13 +9,11 @@
 
 using namespace RedBox;
 
-
-
 Sprite* SpriteFactory::makePolygon(unsigned int nbSides, float sideLength,
 								   const Color& color) {
 	if(nbSides >= 3 && sideLength > 0.0f) {
 		// We calculate the polygon's radius.
-		float radius = sideLength / (2 * sinf(MathHelper::PI/nbSides));
+		float radius = sideLength / (2.0f * sin(MathHelper::PI<float>() / nbSides));
 
 		// Angle from the polygon's center.
 		float angle = 360.0f / nbSides / 2.0f;
@@ -23,8 +21,8 @@ Sprite* SpriteFactory::makePolygon(unsigned int nbSides, float sideLength,
 		Sprite* result = new Sprite();
 		
 		for(unsigned int i = 0; i < nbSides; ++i) {
-			result->createVertex(radius * sinf(MathHelper::degreesToRadians(angle)),
-								 radius * cosf(MathHelper::degreesToRadians(angle)));
+			result->createVertex(radius * sin(MathHelper::degreesToRadians(angle)),
+								 radius * cos(MathHelper::degreesToRadians(angle)));
 			angle += 360.0f / nbSides;
 		}
 
