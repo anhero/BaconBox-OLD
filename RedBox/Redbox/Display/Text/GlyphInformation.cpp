6da6d2ad425789044b0e471306f9ae59feec1b5e
Adam Labranche <adam.labranche@anhero.net>
2011-10-10 16:30:44 -0400

    Created the GlyphInformation class.

---
+++ RedBox/Redbox/Display/Text/GlyphInformation.cpp
@@ -0,0 +1,31 @@
+#include "GlyphInformation.h"
+
+namespace RedBox {
+	GlyphInformation::GlyphInformation() : advance(), horizontalBearing(),
+		size(), textureInformation(NULL) {
+	}
+
+	GlyphInformation::GlyphInformation(const Vector2 &newAdvance,
+	                                   const Vector2 &newHorizontalBearing,
+	                                   const Vector2 &newSize,
+	                                   const TextureInformation *newTextureInformation) :
+		advance(newAdvance), horizontalBearing(newHorizontalBearing),
+		size(newSize), textureInformation(newTextureInformation) {
+	}
+
+	GlyphInformation::GlyphInformation(const GlyphInformation &src) :
+		advance(src.advance), horizontalBearing(src.horizontalBearing),
+		size(src.size), textureInformation(src.textureInformation) {
+	}
+
+	GlyphInformation &GlyphInformation::operator=(const GlyphInformation &src) {
+		if (this != &src) {
+			advance = src.advance;
+			horizontalBearing = src.horizontalBearing;
+			size = src.size;
+			textureInformation = src.textureInformation;
+		}
+
+		return *this;
+	}
+}
