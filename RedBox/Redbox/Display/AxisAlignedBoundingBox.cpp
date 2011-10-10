09e28d5c5237d10e338d584250ad6b2d9d78f80f
Adam Labranche <adam.labranche@anhero.net>
2011-10-10 16:21:21 -0400

    Created the AxisAlignedBoundingBox class.

---
+++ RedBox/Redbox/Display/AxisAlignedBoundingBox.cpp
@@ -0,0 +1,126 @@
+#include "AxisAlignedBoundingBox.h"
+
+#include <algorithm>
+
+namespace RedBox {
+	AxisAlignedBoundingBox::AxisAlignedBoundingBox() : Positionable(), size() {
+	}
+
+	AxisAlignedBoundingBox::AxisAlignedBoundingBox(const Vector2 &newPosition,
+	                                               const Vector2 &newSize) :
+		Positionable(newPosition), size(newSize) {
+	}
+
+	AxisAlignedBoundingBox::AxisAlignedBoundingBox(const AxisAlignedBoundingBox &src) :
+		Positionable(src), size(src.size) {
+	}
+
+	AxisAlignedBoundingBox::~AxisAlignedBoundingBox() {
+	}
+
+	AxisAlignedBoundingBox &AxisAlignedBoundingBox::operator=(const AxisAlignedBoundingBox &src) {
+		this->Positionable::operator=(src);
+
+		if (this != &src) {
+			size = src.size;
+		}
+
+		return *this;
+	}
+
+	const Vector2 AxisAlignedBoundingBox::getSize() const {
+		return size;
+	}
+
+	void AxisAlignedBoundingBox::setSize(const Vector2 &newSize) {
+		size = newSize;
+	}
+
+	void AxisAlignedBoundingBox::setSize(float newWidth, float newHeight) {
+		size.setXY(newWidth, newHeight);
+	}
+
+	float AxisAlignedBoundingBox::getWidth() const {
+		return size.getX();
+	}
+
+	void AxisAlignedBoundingBox::setWidth(float newWidth) {
+		size.setX(newWidth);
+	}
+
+	float AxisAlignedBoundingBox::getHeight() const {
+		return size.getY();
+	}
+
+	void AxisAlignedBoundingBox::setHeight(float newHeight) {
+		size.setY(newHeight);
+	}
+
+	float AxisAlignedBoundingBox::getLeft() const {
+		return std::min(getXPosition(), getXPosition() + size.getX());
+	}
+
+	float AxisAlignedBoundingBox::getRight() const {
+		return std::max(getXPosition(), getXPosition() + size.getX());
+	}
+
+	float AxisAlignedBoundingBox::getTop() const {
+		return std::min(getYPosition(), getYPosition() + size.getY());
+	}
+
+	float AxisAlignedBoundingBox::getBottom() const {
+		return std::max(getYPosition(), getYPosition() + size.getY());
+	}
+
+	bool AxisAlignedBoundingBox::overlaps(const AxisAlignedBoundingBox &other) const {
+		return getRight() > other.getLeft() && getLeft() < other.getRight() &&
+		       getBottom() > other.getTop() && getTop() < other.getBottom();
+	}
+
+	bool AxisAlignedBoundingBox::overlaps(const Vector2 &point) const {
+		return overlaps(point.getX(), point.getY());
+	}
+
+	bool AxisAlignedBoundingBox::overlaps(float xPoint, float yPoint) const {
+		return xPoint >= getLeft() && xPoint <= getRight() &&
+		       yPoint >= getTop() && yPoint <= getBottom();
+	}
+
+	bool AxisAlignedBoundingBox::overlapsHorizontalLine(float yPosition,
+	                                                    float lowerXBoundary,
+	                                                    float higherXBoundary) const {
+		// If the boundaries are reversed.
+		if (lowerXBoundary > higherXBoundary) {
+			// We swap them.
+			float tmp = lowerXBoundary;
+			lowerXBoundary = higherXBoundary;
+			higherXBoundary = tmp;
+		}
+
+		return getBottom() > yPosition && getTop() < yPosition &&
+		       getRight() > lowerXBoundary && getLeft() < higherXBoundary;
+	}
+
+	bool AxisAlignedBoundingBox::overlapsHorizontalLine(float yPosition) const {
+		return getBottom() > yPosition && getTop() < yPosition;
+	}
+
+	bool AxisAlignedBoundingBox::overlapsVerticalLine(float xPosition,
+	                                                  float lowerYBoundary,
+	                                                  float higherYBoundary) const {
+		// If the boundaries are reversed.
+		if (lowerYBoundary > higherYBoundary) {
+			// We swap them.
+			float tmp = lowerYBoundary;
+			lowerYBoundary = higherYBoundary;
+			higherYBoundary = tmp;
+		}
+
+		return getRight() > xPosition && getLeft() < xPosition &&
+		       getBottom() > lowerYBoundary && getTop() < higherYBoundary;
+	}
+
+	bool AxisAlignedBoundingBox::overlapsVerticalLine(float xPosition) const {
+		return getRight() > xPosition && getLeft() < xPosition;
+	}
+}
---
+++ RedBox/Redbox/Display/AxisAlignedBoundingBox.h
@@ -0,0 +1,201 @@
+/**
+ * @file
+ * @ingroup Physics
+ */
+#ifndef RB_AXIS_ALIGNED_BOUNDING_BOX_H
+#define RB_AXIS_ALIGNED_BOUNDING_BOX_H
+
+#include "Positionable.h"
+
+namespace RedBox {
+	/**
+	 * Represents an axis-aligned bounding box.
+	 * @ingroup Physics
+	 */
+	class AxisAlignedBoundingBox : public Positionable {
+	public:
+		/**
+		 * Default constructor.
+		 */
+		AxisAlignedBoundingBox();
+
+		/**
+		 * Parameterized constructor.
+		 * @param newPosition Starting position.
+		 * @param newSize Starting size.
+		 * @see RedBox::Positionable::position
+		 * @see RedBox::AxisAlignedBoundingBox::size
+		 */
+		explicit AxisAlignedBoundingBox(const Vector2 &newPosition,
+		                                const Vector2 &newSize = Vector2());
+
+		/**
+		 * Copy constructor.
+		 * @param src AxisAlignedBoundingBox to make a copy of.
+		 */
+		AxisAlignedBoundingBox(const AxisAlignedBoundingBox &src);
+
+		/**
+		 * Destructor.
+		 */
+		virtual ~AxisAlignedBoundingBox();
+
+		/**
+		 * Assignment operator.
+		 * @param src AxisAlignedBoundingBox to make a copy of.
+		 * @return Reference to the modified AxisAlignedBoundingBox.
+		 */
+		AxisAlignedBoundingBox &operator=(const AxisAlignedBoundingBox &src);
+
+		/**
+		 * Gets the bounding box's size.
+		 * @return Vector2 containing the width and height of the bounding box.
+		 * @see RedBox::AxisAlignedBoundingBox::size
+		 */
+		const Vector2 getSize() const;
+
+		/**
+		 * Sets the bounding box's size.
+		 * @param newSize New size to set.
+		 * @see RedBox::AxisAlignedBoundingBox::size
+		 */
+		void setSize(const Vector2 &newSize);
+
+		/**
+		 * Sets the bounding box's size.
+		 * @param newWidth New width to set.
+		 * @param newHeight New height to set.
+		 * @see RedBox::AxisAlignedBoundingBox::size
+		 */
+		void setSize(float newWidth, float newHeight);
+
+		/**
+		 * Gets the bounding box's width.
+		 * @return Width in pixels (by default).
+		 * @see RedBox::AxisAlignedBoundingBox::size
+		 */
+		float getWidth() const;
+
+		/**
+		 * Sets the bounding box's width.
+		 * @param newWidth New width to set.
+		 * @see RedBox::AxisAlignedBoundingBox::size
+		 */
+		void setWidth(float newWidth);
+
+		/**
+		 * Gets the bounding box's height.
+		 * @return Height in pixels (by default).
+		 * @see RedBox::AxisAlignedBoundingBox::size
+		 */
+		float getHeight() const;
+
+		/**
+		 * Sets the bounding box's height.
+		 * @param newHeight New height to set.
+		 * @see RedBox::AxisAlignedBoundingBox::size
+		 */
+		void setHeight(float newHeight);
+
+		/**
+		 * Gets the bounding box's left side's horizontal coordinate.
+		 * @return Left side's horizontal position.
+		 */
+		float getLeft() const;
+
+		/**
+		 * Gets the bounding box's right side's horizontal coordinate.
+		 * @return Right side's horizontal coordinate.
+		 */
+		float getRight() const;
+
+		/**
+		 * Gets the bounding box's top side's vertical coordinate.
+		 * @return Top side's vertical coordinate.
+		 */
+		float getTop() const;
+
+		/**
+		 * Gets the bounding box's bottom side's vertical coordinate.
+		 * @return Bottom side's vertical coordinate.
+		 */
+		float getBottom() const;
+
+		/**
+		 * Checks if the bounding box overlaps with another bounding box.
+		 * @param other Bounding box to check overlapping with.
+		 * @return True if the two bounding boxes collide, false if not.
+		 */
+		bool overlaps(const AxisAlignedBoundingBox &other) const;
+
+		/**
+		 * Checks if a point overlaps with the bounding box.
+		 * @param point Point to check overlapping with.
+		 * @return True if the point is inside the bounding box, false if not.
+		 */
+		bool overlaps(const Vector2 &point) const;
+
+		/**
+		 * Checks if a point overlaps with the bounding box.
+		 * @param xPoint Horizontal coordinate of the point to check overlapping
+		 * with.
+		 * @param yPoint Vertical coordinate of the point to check overlapping
+		 * with.
+		 * @return True if the point is inside the bounding box, false if not.
+		 */
+		bool overlaps(float xPoint, float yPoint) const;
+
+		/**
+		 * Checks if the bounding box overlaps with a horizontal line. If the
+		 * lower boundary given is higher than the other boundary, they will
+		 * be swapped.
+		 * @param yPosition Vertical position of the horizontal line to check
+		 * overlapping with.
+		 * @param lowerXBoundary Lower horizontal boundary of the line.
+		 * @param higherXBoundary Higher horizontal boundary of the line.
+		 * @return True if the bounding box overlaps with the horizontal line,
+		 * false if not.
+		 */
+		bool overlapsHorizontalLine(float yPosition,
+		                            float lowerXBoundary,
+		                            float higherXBoundary) const;
+
+		/**
+		 * Checks if the bounding box overlaps with a horizontal line.
+		 * @param yPosition Vertical position of the horizontal line to check
+		 * overlapping with.
+		 * @return True if the bounding box overlaps with the horizontal line,
+		 * false if not.
+		 */
+		bool overlapsHorizontalLine(float yPosition) const;
+
+		/**
+		 * Checks if the bounding box overlaps with a vertical line. If the
+		 * lower boundary given is higher than the other boundary, they will
+		 * be swapped.
+		 * @param xPosition Horizontal position of the vertical line to check
+		 * overlapping with.
+		 * @param lowerYBoundary Lower vertical boundary of the line.
+		 * @param higherYBoundary Higher vertical boundary of the line.
+		 * @return True if the bounding box overlaps with the vertical line,
+		 * false if not.
+		 */
+		bool overlapsVerticalLine(float xPosition, float lowerYBoundary,
+		                          float higherYBoundary) const;
+
+		/**
+		 * Checks if the bounding box overlaps with a vertical line.
+		 * @param xPosition Horizontal position of the vertical line to check
+		 * overlapping with.
+		 * @return True if the bounding box overlaps with the vertical line,
+		 * false if not.
+		 */
+		bool overlapsVerticalLine(float xPosition) const;
+	private:
+		/// Size in pixels (by default) of the bounding box.
+		Vector2 size;
+	};
+
+}
+
+#endif
