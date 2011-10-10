2139c9c2b0a0159ba3807563c124dca82065d674
Adam Labranche <adam.labranche@anhero.net>
2011-10-10 16:30:44 -0400

    Wrote the new State class.

--- RedBox/Redbox/State.cpp
+++ RedBox/Redbox/State.cpp
@@ -3,188 +3,117 @@
 #include <cassert>
 #include <algorithm>
 
-#include "GraphicBody.h"
 #include "Console.h"
 #include "GraphicDriver.h"
-#include "DeleteHelper.h"
 
-using namespace RedBox;
+namespace RedBox {
+	const std::string State::DEFAULT_NAME = "State";
 
-const std::string State::DEFAULT_NAME = "State";
+	State::State(const std::string &newName) : Updateable(), Renderable(),
+		BodyManager<Layerable, Layerable::LessCompare>(),
+		sigly::HasSlots<>(), camera(), name(newName) {
+	}
 
-State::State(const std::string& newName) : Object(), sigly::HasSlots<>(),
-	name(newName) {
-}
+	State::~State() {
+	}
 
-State::~State() {
-	// We delete all the graphic bodies from the list of GraphicBodys that need
-	// to be deleted.
-	std::for_each(toDelete.begin(), toDelete.end(), DeletePointer());
+	void State::update() {
+	}
 
-	// We delete the graphic bodies that were waiting to be added.
-	std::for_each(toAdd.begin(), toAdd.end(), DeletePointer());
+	void State::render() {
+	}
 
-	// We delete the graphic bodies.
-	std::for_each(graphicBodies.begin(), graphicBodies.end(),
-	              DeletePointerFromPair());
-}
+	const std::string &State::getName() const {
+		return name;
+	}
 
-void State::addGraphicBody(GraphicBody* aGraphicBody) {
-	if(aGraphicBody) {
-		if(!aGraphicBody->isInState) {
-			toAdd.push_front(aGraphicBody);
-		} else {
-			Console::println("Tried to add a GraphicBody that is already in a state.");
+	void State::setName(const std::string &newName) {
+		name = newName;
+	}
+
+	const Color &State::getBackgroundColor() const {
+		return camera.getBackgroundColor();
+	}
+
+	void State::setBackgroundColor(const Color &newBackgroundColor) {
+		camera.setBackgroundColor(newBackgroundColor);
+	}
+
+	const Camera &State::getCamera() const {
+		return camera;
+	}
+
+	void State::onGetFocus() {
+	}
+
+	void State::onLoseFocus() {
+	}
+
+	void State::internalUpdate() {
+		this->BodyManager<Layerable, Layerable::LessCompare>::internalUpdate();
+
+		if (camera.isEnabled() && camera.isActive()) {
+			camera.update();
 		}
-	} else {
-		Console::println("Tried to add an invalid GraphicBody (" + Console::toString(aGraphicBody) + ") to the state.");
+
+		update();
 	}
-}
 
+	void State::internalRender() {
+		if (camera.isEnabled() && camera.isVisible()) {
+			camera.render();
+		}
 
-void State::render() {
-}
+		// We check if there are bodies to be rendered.
+		if (!bodies.empty()) {
+			BodyMap::iterator i = bodies.begin();
+			// We get the first body's scroll factor.
+			Vector2 lastScrollFactor = (*i)->getScrollFactor();
+			// We apply the scroll factor.
+			GraphicDriver::getInstance().pushMatrix();
+			GraphicDriver::getInstance().translate(Vector2(-(1.0f - lastScrollFactor.getX()) * camera.getXPosition(),
+			                                               -(1.0f - lastScrollFactor.getY()) * camera.getYPosition()));
 
-void State::update() {
-}
+			// For each body.
+			while (i != bodies.end()) {
+				// We make sure it is enabled and visible.
+				if ((*i)->isEnabled() && (*i)->isVisible()) {
+					// We check if it has a scroll factor different than the
+					// one already in place.
+					if (lastScrollFactor != (*i)->getScrollFactor()) {
+						// We remove the old scroll factor apply the new scroll
+						// factor.
+						lastScrollFactor = (*i)->getScrollFactor();
+						GraphicDriver::getInstance().popMatrix();
+						GraphicDriver::getInstance().pushMatrix();
+						GraphicDriver::getInstance().translate(Vector2(-(1.0f - lastScrollFactor.getX()) * camera.getXPosition(),
+						                                               -(1.0f - lastScrollFactor.getY()) * camera.getYPosition()));
+					}
 
-const std::string& State::getName() const {
-	return name;
-}
-
-void State::setName(const std::string& newName) {
-	name = newName;
-}
-
-void State::setBackgroundColor(const Color& newBackgroundColor) {
-	camera.setBackgroundColor(newBackgroundColor);
-}
-
-const Color& State::getBackgroundColor() const {
-	return camera.getBackgroundColor();
-}
-
-void State::onGetFocus() {
-}
-
-void State::onLoseFocus() {
-}
-
-void State::addGraphicBodyDirect(GraphicBody* aGraphicBody) {
-	assert(aGraphicBody);
-
-	if(!aGraphicBody->isInState) {
-		aGraphicBody->resetLayerChanged();
-		graphicBodies.insert(std::pair<Layer, GraphicBody*>(aGraphicBody->getLayer(),
-		                     aGraphicBody));
-		aGraphicBody->isInState = true;
-	} else {
-		Console::println("Tried to add a GraphicBody that is already in a state.");
-	}
-}
-
-void State::internalUpdate() {
-	// We add the graphic bodies to the multimap.
-	std::for_each(toAdd.begin(), toAdd.end(), std::bind1st(std::mem_fun(&State::addGraphicBodyDirect), this));
-	// We clear the temporary list of graphic bodies to add.
-	toAdd.clear();
-
-	// We update the graphic bodies.
-	BodyMap::iterator i = graphicBodies.begin();
-	while(i != graphicBodies.end()) {
-		// We check if the delete flag is on.
-		if(i->second->isToBeDeleted()) {
-			// We put the GraphicBody in the list of GraphicBodys to delete.
-			toDelete.push_back(i->second);
-			// We remove the GraphicBody from the multimap.
-			graphicBodies.erase(i++);
-		} else {
-			if(i->second->isEnabled() && i->second->isActive()) {
-				// We update the GraphicBody.
-				i->second->update();
-
-				// We check if the z value has changed.
-				if(i->second->isLayerChanged()) {
-					// If so, we put it at the right layer in the multimap.
-					// To do that, we first put it in the list of GraphicBodys that
-					// have had their z changed.
-					layerChange.push_back(i->second);
-					// We remove it from the multimap.
-					graphicBodies.erase(i++);
-				} else {
-					++i;
+					// We render the body.
+					(*i)->render();
 				}
-			} else {
+
 				++i;
 			}
+
+			GraphicDriver::getInstance().popMatrix();
+		}
+
+		render();
+
+		if (!(camera.isEnabled() && camera.isVisible())) {
+			camera.render();
 		}
 	}
 
-	// We delete all the graphic bodies from the list of GraphicBodys that need
-	// to be deleted.
-	std::for_each(toDelete.begin(), toDelete.end(), DeletePointer());
-	// We clear the list.
-	toDelete.clear();
-
-	// Put the graphic bodies which have had their z changed back into the
-	// multimap.
-	for(std::list<GraphicBody*>::iterator i = layerChange.begin();
-	    i != layerChange.end();
-		++i) {
-		graphicBodies.insert(std::pair<Layer, GraphicBody*>((*i)->getLayer(), *i));
+	void State::internalOnGetFocus() {
+		activateSlots();
+		onGetFocus();
 	}
 
-	layerChange.clear();
-
-	if(camera.isEnabled() && camera.isActive()) {
-		camera.update();
+	void State::internalOnLoseFocus() {
+		deactivateSlots();
+		onLoseFocus();
 	}
-
-	update();
-}
-
-void State::internalRender() {
-	if(camera.isEnabled() && camera.isVisible()) {
-		camera.render();
-	}
-
-	if(!graphicBodies.empty()) {
-		Layer lastLayer = graphicBodies.begin()->first;
-		GraphicDriver::pushMatrix();
-		GraphicDriver::translate(Vector2(-(1.0f - graphicBodies.begin()->second->getLayer().getScrollFactor().getX()) * camera.getXPosition(),
-		                              -(1.0f - graphicBodies.begin()->second->getLayer().getScrollFactor().getY()) * camera.getYPosition()));
-
-		for(BodyMap::iterator i = graphicBodies.begin();
-			i != graphicBodies.end(); ++i) {
-			if(i->second->isEnabled() && i->second->isVisible()) {
-				if(lastLayer != i->first) {
-					GraphicDriver::popMatrix();
-					GraphicDriver::pushMatrix();
-					GraphicDriver::translate(Vector2(-(1.0f - i->second->getLayer().getScrollFactor().getX()) * camera.getXPosition(),
-					                              -(1.0f - i->second->getLayer().getScrollFactor().getY()) * camera.getYPosition()));
-					lastLayer = i->first;
-				}
-
-				i->second->render();
-			}
-		}
-
-		GraphicDriver::popMatrix();
-	}
-
-	render();
-
-	if(!(camera.isEnabled() && camera.isVisible())) {
-		camera.render();
-	}
-}
-
-void State::internalOnGetFocus() {
-	activateSlots();
-	onGetFocus();
-}
-
-void State::internalOnLoseFocus() {
-	deactivateSlots();
-	onLoseFocus();
 }
--- RedBox/Redbox/State.h
+++ RedBox/Redbox/State.h
@@ -4,26 +4,26 @@
 #ifndef RB_STATE_H
 #define RB_STATE_H
 
-#include <map>
-#include <list>
 #include <string>
 
 #include <sigly.h>
 
-#include "Object.h"
-#include "Layer.h"
 #include "Camera.h"
 #include "Color.h"
+#include "BodyManager.h"
+#include "Layerable.h"
+#include "Renderable.h"
+#include "Updateable.h"
 
 namespace RedBox {
-	class GraphicBody;
 	/**
 	 * A state represents the the game's different states, it contains and
-	 * manages the GraphicBody objects. Ex: the playState, the MenuState.
-     */
-	class State : public Object, public sigly::HasSlots<> {
+	 * manages the Layerable objects. Ex: the PlayState, the MenuState.
+	 */
+	class State : public Updateable, public Renderable,
+		public BodyManager<Layerable, Layerable::LessCompare>,
+		sigly::HasSlots<> {
 		friend class Engine;
-		friend class DebugState;
 	public:
 		/**
 		 * Default name given to states initialized with the default
@@ -35,32 +35,12 @@ namespace RedBox {
 		 * Simple parameterized constructor and default constructor.
 		 * @param newName Initial name to give to the state.
 		 */
-		State(const std::string& newName = DEFAULT_NAME);
+		State(const std::string &newName = DEFAULT_NAME);
 
 		/**
 		 * Destructor.
 		 */
 		virtual ~State();
-
-		/**
-         * Adds a GraphicBody object to the multimap to begin rendering and
-		 * updating it. Once a GraphicBody is added with this function, it is
-		 * added in a temporary list of GraphicBodys that will be added on the
-		 * next update. The GraphicBody's destruction is managed by the state.
-         * @param aGraphicBody Pointer to the GraphicBody to add to the state's
-		 * GraphicBodys.
-         */
-		void addGraphicBody(GraphicBody* aGraphicBody);
-
-		/**
-		 * Renders all the objects that need to be rendered. You have to
-		 * overload this method if you want to add custom code to it. You don't
-		 * have to call the parent render function inside your implementation.
-		 * The internal render code happens in internalRender, which then calls
-		 * this render method.
-		 * @see RedBox::State::internalRender()
-         */
-        virtual void render();
 
 		/**
 		 * Deletes, Updates and moves all concerns objects. You have to
@@ -69,30 +49,32 @@ namespace RedBox {
 		 * The internal update code happens in internalUpdate, which then calls
 		 * this update method.
 		 * @see RedBox::State::internalUpdate()
-         */
-        virtual void update();
+		 */
+		virtual void update();
+
+		/**
+		 * Renders all the objects that need to be rendered. You have to
+		 * overload this method if you want to add custom code to it. You don't
+		 * have to call the parent render function inside your implementation.
+		 * The internal render code happens in internalRender, which then calls
+		 * this render method.
+		 * @see RedBox::State::internalRender()
+		 */
+		virtual void render();
 
 		/**
 		 * Gets the state's name.
 		 * @return State's name.
 		 * @see RedBox::State::name
 		 */
-		const std::string& getName() const;
+		const std::string &getName() const;
 
 		/**
 		 * Sets the state's name.
 		 * @param newName State's new name.
 		 * @see RedBox::State::name
 		 */
-		void setName(const std::string& newName);
-
-		/**
-		 * Sets the state's background color.
-		 * @param newBackgroundColor State's new background color.
-		 * @see RedBox::State::camera
-		 * @see RedBox::Camera::backgroundColor
-		 */
-		void setBackgroundColor(const Color& newBackgroundColor);
+		void setName(const std::string &newName);
 
 		/**
 		 * Gets the state's background color.
@@ -100,8 +82,22 @@ namespace RedBox {
 		 * @see RedBox::State::camera
 		 * @see RedBox::Camera::backgroundColor
 		 */
-		const Color& getBackgroundColor() const;
+		const Color &getBackgroundColor() const;
 
+		/**
+		 * Sets the state's background color.
+		 * @param newBackgroundColor State's new background color.
+		 * @see RedBox::State::camera
+		 * @see RedBox::Camera::backgroundColor
+		 */
+		void setBackgroundColor(const Color &newBackgroundColor);
+
+		/**
+		 * Gets the state's camera.
+		 * @return The state's camera.
+		 * @see RedBox::State::camera
+		 */
+		const Camera &getCamera() const;
 	protected:
 		/// The camera object which prepare the scene before rendering any object.
 		Camera camera;
@@ -119,33 +115,9 @@ namespace RedBox {
 		 */
 		virtual void onLoseFocus();
 	private:
-		/// Container type for the graphic bodies.
-		typedef std::multimap<Layer, GraphicBody*, Layer::LessCompare> BodyMap;
-
-		/// Temporarily stores the GraphicBody objects to be deleted.
-        std::list<GraphicBody*> toDelete;
-
-		/// Temporarily stores the GraphicBody objects to be added.
-		std::list<GraphicBody*> toAdd;
-
-		/**
-		 * Temporarily stores the GraphicBody objects that need to change their
-		 * Z value.
-		 */
-		std::list<GraphicBody*> layerChange;
-
-		/// Stores all the GraphicBodys active GraphicBodys objects.
-		BodyMap graphicBodies;
 
 		/// State's name, used as an identifier.
-        std::string name;
-		
-		/**
-		 * Adds a GraphicBody in the multimap of GraphicBodys. It will then be
-		 * able to be updated and rendered correctly.
-		 * @param aGraphicBody Pointer to the GraphicBody to add.
-		 */
-		void addGraphicBodyDirect(GraphicBody* aGraphicBody);
+		std::string name;
 
 		/**
 		 * Private update method called by the Engine class.
