eefcdf19826675fbeb14742e6db476d023f3f5ef
Adam Labranche <adam.labranche@anhero.net>
2011-10-10 16:30:44 -0400

    Wrote the IsSame class.

---
+++ RedBox/Redbox/Helper/IsSame.h
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * @ingroup Helper
+ */
+#ifndef RB_IS_SAME_H
+#define RB_IS_SAME_H
+
+namespace RedBox {
+	/**
+	 * Used to statically check if two types are the same.
+	 * @tparam T First type to compare.
+	 * @tparam S Second type to compare.
+	 * @ingroup Helper
+	 */
+	template <typename T, typename S>
+	struct IsSame {
+		static const bool RESULT = false;
+	};
+
+	template <typename T>
+	struct IsSame<T, T> {
+		static const bool RESULT = true;
+	};
+}
+
+#endif // RB_IS_SAME_H
