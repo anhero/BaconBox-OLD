6f3c1ed863ea53955f684466099cdf2386e7717d
Adam Labranche <adam.labranche@anhero.net>
2011-10-10 16:21:18 -0400

    Created the Hidable class.

---
+++ RedBox/Redbox/Display/Hidable.cpp
@@ -0,0 +1,28 @@
+#include "Hidable.h"
+
+namespace RedBox {
+	Hidable::Hidable() : Maskable(), visible(true) {
+	}
+
+	Hidable::Hidable(const Hidable &src) : Maskable(src),
+	    visible(src.visible) {
+	}
+
+	Hidable::~Hidable() {
+	}
+
+	Hidable &Hidable::operator=(const Hidable &src) {
+		if(this != &src) {
+			visible = src.visible;
+		}
+		return *this;
+	}
+
+	bool Hidable::isVisible() const {
+		return visible;
+	}
+
+	void Hidable::setVisible(bool newVisible) {
+		visible = newVisible;
+	}
+}
