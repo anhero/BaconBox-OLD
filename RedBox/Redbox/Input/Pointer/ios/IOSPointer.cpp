72f6a00a5e52d0b6a0714ffbff9e410cca050f82
Adam Labranche <adam.labranche@anhero.net>
2011-11-01 15:19:18 -0400

    Re-fixed the IOSPointer class.

--- RedBox/Redbox/Input/Pointer/ios/IOSPointer.cpp
+++ RedBox/Redbox/Input/Pointer/ios/IOSPointer.cpp
@@ -1,115 +1,146 @@
 #include "IOSPointer.h"
 #include "CursorButton.h"
-#define RB_MAX_IOS_NB_OF_TOUCH 11
-using namespace RedBox;
+#include "MainWindow.h"
 
-sigly::Signal2<NSSet*, UIEvent*> IOSPointer::touchBegin = sigly::Signal2<NSSet*, UIEvent*>();
-sigly::Signal2<NSSet*, UIEvent*> IOSPointer::touchEnd = sigly::Signal2<NSSet*, UIEvent*>();
-sigly::Signal2<NSSet*, UIEvent*> IOSPointer::touchMove = sigly::Signal2<NSSet*, UIEvent*>();
-sigly::Signal2<NSSet*, UIEvent*> IOSPointer::touchCancelled = sigly::Signal2<NSSet*, UIEvent*>();
+namespace RedBox {
+	const unsigned int MAX_IOS_NB_OF_TOUCH = 11;
 
-IOSPointer::IOSPointer(): Pointer(RB_MAX_IOS_NB_OF_TOUCH), sigly::HasSlots<>(), touches(RB_MAX_IOS_NB_OF_TOUCH) {
-	touchBegin.connect(this, &IOSPointer::onTouchBegin);
-	touchEnd.connect(this, &IOSPointer::onTouchEnd);
-	touchMove.connect(this, &IOSPointer::onTouchMove);
-    touchCancelled.connect(this, &IOSPointer::onTouchCancelled);
+	sigly::Signal2<NSSet *, UIEvent *> IOSPointer::touchBegin = sigly::Signal2<NSSet *, UIEvent *>();
+	sigly::Signal2<NSSet *, UIEvent *> IOSPointer::touchEnd = sigly::Signal2<NSSet *, UIEvent *>();
+	sigly::Signal2<NSSet *, UIEvent *> IOSPointer::touchMove = sigly::Signal2<NSSet *, UIEvent *>();
+	sigly::Signal2<NSSet *, UIEvent *> IOSPointer::touchCancelled = sigly::Signal2<NSSet *, UIEvent *>();
+
+	IOSPointer::IOSPointer(): Pointer(MAX_IOS_NB_OF_TOUCH), sigly::HasSlots<>(), touches(MAX_IOS_NB_OF_TOUCH) {
+		touchBegin.connect(this, &IOSPointer::onTouchBegin);
+		touchEnd.connect(this, &IOSPointer::onTouchEnd);
+		touchMove.connect(this, &IOSPointer::onTouchMove);
+		touchCancelled.connect(this, &IOSPointer::onTouchCancelled);
+	}
+
+	IOSPointer::~IOSPointer() {
+	}
+
+	void IOSPointer::updateDevice() {
+		for (unsigned int i = 0;  i < MAX_IOS_NB_OF_TOUCH; i++) {
+			getCursorPreviousPosition(i) = getCursorPosition(i);
+			getCursorPosition(i) = this->touches[i].position;
+			getCursorPreviousButtons(i)[CursorButton::LEFT] = getCursorButtons(i)[CursorButton::LEFT];
+			getCursorButtons(i)[CursorButton::LEFT] = this->touches[i].inContact;
+
+			if (isButtonPressed(CursorButton::LEFT, i)) {
+				buttonPress(PointerButtonSignalData(state, i, CursorButton::LEFT));
+
+			} else if (isButtonHeld(CursorButton::LEFT, i)) {
+				buttonHold(PointerButtonSignalData(state, i, CursorButton::LEFT));
+
+			} else if (isButtonReleased(CursorButton::LEFT, i)) {
+				buttonRelease(PointerButtonSignalData(state, i, CursorButton::LEFT));
+			}
+
+			if (hasMoved(i)) {
+				Pointer::move.shoot(PointerSignalData(state, i));
+			}
+		}
+
+
+	}
+
+	void IOSPointer::convertToRedBoxScreenPosition(Vector2 *newIosPosition) {
+		if (std::max(MainWindow::getInstance().getResolutionWidth(), MainWindow::getInstance().getResolutionHeight()) == 960) {
+			newIosPosition->scalarMultiplication(2.0f);
+		}
+
+		switch (MainWindow::getInstance().getOrientation().underlying()) {
+		case WindowOrientation::UPSIDE_DOWN:
+			newIosPosition->setY(MainWindow::getInstance().getResolutionHeight() - newIosPosition->getY());
+			break;
+
+		case WindowOrientation::HORIZONTAL_LEFT:
+			newIosPosition->setXY(newIosPosition->getY(),
+			                      MainWindow::getInstance().getResolutionHeight() - newIosPosition->getX());
+			break;
+
+		case WindowOrientation::HORIZONTAL_RIGHT:
+			newIosPosition->setXY(MainWindow::getInstance().getResolutionWidth() - newIosPosition->getY(),
+			                      newIosPosition->getX());
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	int IOSPointer::AddNewTouch(UITouch *touch) {
+		for (unsigned int i = 0; i < MAX_IOS_NB_OF_TOUCH; i++) {
+			if (touches[i].touch == NULL) {
+				touches[i].touch = touch;
+				return  i;
+			}
+		}
+
+		return -1;
+	}
+
+	int IOSPointer::getTouchID(UITouch *touch) {
+		for (unsigned int i = 0; i < MAX_IOS_NB_OF_TOUCH; i++) {
+			if (touches[i].touch == touch) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	void IOSPointer::onTouchBegin(NSSet *touches, UIEvent *event) {
+		for (UITouch * touch in touches) {
+			int touchID = getTouchID(touch);
+
+			if (touchID == -1) {
+				touchID = AddNewTouch(touch);
+			}
+
+			if (touchID != -1) {
+				CGPoint location = [touch locationInView: [touch view]];
+				this->touches[touchID].position =  Vector2(static_cast<float>(location.x), static_cast<float>(location.y));
+				convertToRedBoxScreenPosition(&(this->touches[touchID].position));
+				this->touches[touchID].inContact = true;
+			}
+		}
+
+	}
+
+	void IOSPointer::onTouchEnd(NSSet *touches, UIEvent *event) {
+		for (UITouch * touch in touches) {
+			int touchID = getTouchID(touch);
+
+			if (touchID != -1) {
+				this->touches[touchID].touch = NULL;
+
+				CGPoint location = [touch locationInView: [touch view]];
+				this->touches[touchID].position = Vector2(static_cast<float>(location.x), static_cast<float>(location.y));
+				convertToRedBoxScreenPosition(&(this->touches[touchID].position));
+				this->touches[touchID].inContact = false;
+			}
+
+
+		}
+	}
+
+	void IOSPointer::onTouchCancelled(NSSet *touches, UIEvent *event) {
+		onTouchEnd(touches, event);
+	}
+
+	void IOSPointer::onTouchMove(NSSet *touches, UIEvent *event) {
+		for (UITouch * touch in touches) {
+			int touchID = getTouchID(touch);
+
+			if (touchID != -1) {
+				CGPoint location = [touch locationInView: [touch view]];
+				this->touches[touchID].position =  Vector2(static_cast<float>(location.x), static_cast<float>(location.y));
+				convertToRedBoxScreenPosition(&(this->touches[touchID].position));
+				this->touches[touchID].inContact = true;
+			}
+		}
+
+	}
 }
-
-IOSPointer::~IOSPointer() {
-}
-
-void IOSPointer::updateDevice() {
-    
-    
-    for (unsigned int i = 0;  i < RB_MAX_IOS_NB_OF_TOUCH; i++) {
-        getCursorPreviousPosition(i) = getCursorPosition(i);
-        getCursorPosition(i) = this->touches[i].position;
-        getCursorPreviousButtons(i)[CursorButton::LEFT] = getCursorButtons(i)[CursorButton::LEFT];
-        getCursorButtons(i)[CursorButton::LEFT] = this->touches[i].inContact;
-        
-        
-        if(isButtonPressed(CursorButton::LEFT, i)) {
-            buttonPress(PointerButtonSignalData(state, i, CursorButton::LEFT));
-        } 
-        else if(isButtonHeld(CursorButton::LEFT, i)) {
-            buttonHold(PointerButtonSignalData(state, i, CursorButton::LEFT));
-        } 
-        else if(isButtonReleased(CursorButton::LEFT, i)) {
-            buttonRelease(PointerButtonSignalData(state, i, CursorButton::LEFT));
-        }
-        if(hasMoved(i)) {
-            Pointer::move.shoot(PointerSignalData(state, i));
-        }
-    }
-	
-	
-}
-
-int IOSPointer::AddNewTouch(UITouch* touch){
-    for (unsigned int i = 0; i < RB_MAX_IOS_NB_OF_TOUCH; i++) {
-        if(touches[i].touch == NULL){
-            touches[i].touch = touch;
-            return  i;
-        }
-    }
-    return -1;
-}
-
-int IOSPointer::getTouchID(UITouch* touch){
-    for (unsigned int i = 0; i < RB_MAX_IOS_NB_OF_TOUCH; i++) {
-        if (touches[i].touch == touch) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-void IOSPointer::onTouchBegin(NSSet* touches, UIEvent* event) {
-	for(UITouch * touch in touches){
-        int touchID = getTouchID(touch);
-        
-        if (touchID == -1){
-            touchID = AddNewTouch(touch);
-        }
-        if(touchID != -1){
-            CGPoint location = [touch locationInView: [touch view]];
-            this->touches[touchID].position =  Vector2(static_cast<float>(location.x), static_cast<float>(location.y) );
-            this->touches[touchID].inContact = true;
-        }
-    }
-    
-}
-
-void IOSPointer::onTouchEnd(NSSet* touches, UIEvent* event) {
-	for(UITouch * touch in touches){
-        int touchID = getTouchID(touch);
-        
-        if (touchID != -1){
-            this->touches[touchID].touch = NULL;
-            
-            CGPoint location = [touch locationInView: [touch view]];
-            this->touches[touchID].position =  Vector2(static_cast<float>(location.x), static_cast<float>(location.y) );
-            this->touches[touchID].inContact = false;
-        }
-        
-        
-    }
-}
-
-void IOSPointer::onTouchCancelled(NSSet* touches, UIEvent* event){
-    onTouchEnd(touches, event);
-}
-
-void IOSPointer::onTouchMove(NSSet* touches, UIEvent* event) {
-	for(UITouch * touch in touches){
-        int touchID = getTouchID(touch);
-        
-        if(touchID != -1){
-            CGPoint location = [touch locationInView: [touch view]];
-            this->touches[touchID].position =  Vector2(static_cast<float>(location.x), static_cast<float>(location.y) );
-            this->touches[touchID].inContact = true;
-        }
-    }
-    
-}
-
--- RedBox/Redbox/Input/Pointer/ios/IOSPointer.h
+++ RedBox/Redbox/Input/Pointer/ios/IOSPointer.h
@@ -20,90 +20,91 @@ namespace RedBox {
 	 */
 	class IOSPointer : public Pointer, public sigly::HasSlots<> {
 		friend class InputManager;
-		friend class sigly::Signal2<NSSet*, UIEvent*>;
+		friend class sigly::Signal2<NSSet *, UIEvent *>;
 	public:
 		/// Signal sent by EAGLView when a touch begins.
-		static sigly::Signal2<NSSet*, UIEvent*> touchBegin;
+		static sigly::Signal2<NSSet *, UIEvent *> touchBegin;
+		
 		/// Signal sent by EAGLView when a touch ends.
-		static sigly::Signal2<NSSet*, UIEvent*> touchEnd;
+		static sigly::Signal2<NSSet *, UIEvent *> touchEnd;
+		
 		/// Signal sent by EAGLView when a touch moves.
-		static sigly::Signal2<NSSet*, UIEvent*> touchMove;
-        /// Signal sent by EAGLView when a touch is cancelled.
-		static sigly::Signal2<NSSet*, UIEvent*> touchCancelled;
+		static sigly::Signal2<NSSet *, UIEvent *> touchMove;
+		
+		/// Signal sent by EAGLView when a touch is cancelled.
+		static sigly::Signal2<NSSet *, UIEvent *> touchCancelled;
 	protected:
 		/**
 		 * Default constructor.
 		 */
 		IOSPointer();
+		
 		/**
 		 * Destructor.
 		 */
 		~IOSPointer();
+		
 		/**
 		 * Updates the input device information.
 		 */
 		void updateDevice();
 	private:
-		/// Temporary position for ios' event handling.
-		Vector2 iosPosition;
-		/// Temporary button state for ios' event handling.
-		bool isTouchDown;
-		
 		/**
 		 * Converts the position we receive to RedBox's format.
 		 * @param newIosPosition Pointer to the vector to set the RedBox
 		 * screen position.
 		 */
 		static void convertToRedBoxScreenPosition(Vector2 *newIosPosition);
-		
 
 		/**
 		 * Called when EAGLView gets a touchesBegan event.
 		 */
-		void onTouchBegin(NSSet* touches, UIEvent* event);
+		void onTouchBegin(NSSet *touches, UIEvent *event);
+
 		/**
 		 * Called when EAGLView gets a touchesEnded event.
 		 */
-		void onTouchEnd(NSSet* touches, UIEvent* event);
+		void onTouchEnd(NSSet *touches, UIEvent *event);
+
 		/**
 		 * Called when EAGLView gets a touchesMoved event.
 		 */
-		void onTouchMove(NSSet* touches, UIEvent* event);
-        /**
+		void onTouchMove(NSSet *touches, UIEvent *event);
+
+		/**
 		 * Called when EAGLView gets a touchesCancelled event.
 		 */
-        void onTouchCancelled(NSSet* touches, UIEvent* event);
-        
-        
-        struct IOSTouch {
-            IOSTouch():touch(NULL), inContact(false), position(0.0f,0.0f){}
-            ///Pointer to the IOS touch object
-            UITouch * touch;
-            
-            ///Last known position for this touch event.
-            Vector2 position;
-            ///True if the touch is currently happening, false if the touch is ended.
-            bool inContact;
-        };
-        
-        /**
-         * Used for multi touch tracking; it matches the touch object with their cursor ID. 
-         * (The key of each object in the vector is the matching cursor ID.)
-         */
-        std::vector<IOSTouch> touches;
+		void onTouchCancelled(NSSet *touches, UIEvent *event);
 
-        
-        /**
-         * Add the given touch object and match it to a cursor.
-         * @return Return the matching cursor ID and -1 if it can't add the given touch object.
-         */
-        int AddNewTouch(UITouch* touch);
-        
-        /**
-         * Return the matching cursor ID for the given touch object.
-         * @return The matching cursor ID or -1 if there is no matching cursor.
-         */
-        int getTouchID(UITouch* touch);
+
+		struct IOSTouch {
+			IOSTouch(): touch(NULL), inContact(false), position(0.0f, 0.0f) {}
+			///Pointer to the IOS touch object
+			UITouch *touch;
+
+			///Last known position for this touch event.
+			Vector2 position;
+			///True if the touch is currently happening, false if the touch is ended.
+			bool inContact;
+		};
+
+		/**
+		 * Add the given touch object and match it to a cursor.
+		 * @return Return the matching cursor ID and -1 if it can't add the given touch object.
+		 */
+		int AddNewTouch(UITouch *touch);
+
+		/**
+		 * Return the matching cursor ID for the given touch object.
+		 * @return The matching cursor ID or -1 if there is no matching cursor.
+		 */
+		int getTouchID(UITouch *touch);
+
+		/**
+		 * Used for multi touch tracking; it matches the touch object with their cursor ID.
+		 * (The key of each object in the vector is the matching cursor ID.)
+		 */
+		std::vector<IOSTouch> touches;
 	};
 }
 
