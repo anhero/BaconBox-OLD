6453273a5c893b67c2fc692b11efa58c7db18450
Samuel Dionne-Riel <samuel@dionne-riel.com>
2012-06-15 18:45:31 -0400

    Fixed the Gamepads to actually support the other axis.

--- RedBox/Redbox/Input/GamePad/SDL/SDLGamePad.cpp
+++ RedBox/Redbox/Input/GamePad/SDL/SDLGamePad.cpp
@@ -32,7 +32,7 @@ namespace RedBox {
             }
         }
         
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < getThumbstick().size(); i++) {
             float  axisValue = SDL_JoystickGetAxis(joystick, i);
             float finalAxisValue;
             if(axisValue < 0){
